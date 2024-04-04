da32e1f78ade7737acaa7fc72baf9d7df5cf7941
Tam Trang <tuo98490@temple.edu>
2024-04-04 12:14:34 -0400

    Added button click to start the service

--- app/src/main/java/edu/temple/startserviceinclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/startserviceinclassactivity/MainActivity.kt
@@ -1,10 +1,12 @@
 package edu.temple.startserviceinclassactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 
+const val COUNTDOWN_START_VALUE = "countdownvalue"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +16,10 @@ class MainActivity : AppCompatActivity() {
 
         val countdownStartButton = findViewById<Button>(R.id.countdownButton)
 
+
+        countdownStartButton.setOnClickListener {
+            startService(Intent(this, CountdownService::class.java)
+                .putExtra(COUNTDOWN_START_VALUE, countdownValue.text.toString().toInt()))
+        }
     }
 }
